48e96bd9df7ecc6bf50a3399b333d9e4eaeea333
Josh <jdavid@phoceis.com>
2016-05-31 13:46:17 +0200

    Update version gradle Add timepicker

--- whateverapi/src/main/java/com/github/quentin7b/whateverapi/android/view/WEApiView.java
+++ whateverapi/src/main/java/com/github/quentin7b/whateverapi/android/view/WEApiView.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.TimePicker;
 
 /**
  * Helper for http://developer.android.com/reference/android/view/View.html
@@ -42,6 +43,68 @@ public class WEApiView {
         } else {
             view.setBackgroundDrawable(background);
         }
+    }
+
+
+    /***
+     * Set current hour to {@link TimePicker}
+     *
+     * @param timePicker
+     * @param hour
+     */
+    public static void setTimePickerHour(@NonNull TimePicker timePicker, int hour) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            timePicker.setHour(hour);
+        } else {
+            timePicker.setCurrentHour(hour);
+        }
+    }
+
+
+    /***
+     * Set minute to {@link TimePicker}
+     *
+     * @param timePicker
+     * @param minute
+     */
+    public static void setTimePickerMinute(@NonNull TimePicker timePicker, int minute) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            timePicker.setMinute(minute);
+        } else {
+            timePicker.setCurrentMinute(minute);
+        }
+    }
+
+    /**
+     * Get the current hour from {@link TimePicker}
+     *
+     * @param timePicker
+     * @return
+     */
+    public static int getTimePickerHour(@NonNull TimePicker timePicker) {
+        int hour = 0;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            hour = timePicker.getHour();
+        } else {
+            hour = timePicker.getCurrentHour();
+        }
+        return hour;
+    }
+
+    /***
+     * Get the current minute from {@link TimePicker}
+     *
+     * @param timePicker
+     * @return
+     */
+    public static int getTimePickerMinute(@NonNull TimePicker timePicker) {
+        int minute = 0;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            minute = timePicker.getMinute();
+        } else {
+            minute = timePicker.getCurrentMinute();
+        }
+        return minute;
     }
 
 }
